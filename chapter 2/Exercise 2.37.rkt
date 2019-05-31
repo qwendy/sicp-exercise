dbd670ccdf67fb00b8ef23f0da23ef90fa7cc502
皆休 <1542997629@qq.com>
2019-05-31 18:23:05 +0800

    2.37,to be continued

--- chapter 2/Exercise 2.37.rkt
+++ chapter 2/Exercise 2.37.rkt
@@ -3,3 +3,8 @@
 (define (dot-product v w)
     (accumulate + 0 (map * v w)))
 
+(define (matrix-*-vector m v) (map (lambda (x) (map (lambda (y) (* y )) x) m))
+
+(define (transpose mat) (accumulate-n ⟨??⟩ ⟨??⟩ mat))
+
+( define (matrix-*-matrix m n) (let ((cols (transpose n))) (map ⟨??⟩ m)))
