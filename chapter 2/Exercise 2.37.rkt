500aeed975470833e9e7c792a5bcb3549cef434f
qianwenxiang <1542997629@qq.com>
2019-06-03 01:17:23 +0800

    finished 2.37

--- chapter 2/Exercise 2.37.rkt
+++ chapter 2/Exercise 2.37.rkt
@@ -1,10 +1,47 @@
 #lang sicp
+(define (accumulate f null-value seq)
+    (if (null? seq)
+        null-value
+        (begin
+                (f  (car seq)
+                    (accumulate f null-value (cdr seq))))
+        ))
+
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        nil
+        (cons   (accumulate op init (map (lambda (x) (car x)) seqs))
+                (accumulate-n op init (map (lambda (x) (cdr x)) seqs))))
+)
 
 (define (dot-product v w)
     (accumulate + 0 (map * v w)))
 
-(define (matrix-*-vector m v) (map (lambda (x) (map (lambda (y) (* y )) x) m))
+(define (matrix-*-vector m v)
+    (map (lambda (row) (dot-product row v)) m))
 
-(define (transpose mat) (accumulate-n ⟨??⟩ ⟨??⟩ mat))
+(define m
+    (list   (list 1 2 3 4)
+            (list 4 5 6 6)
+            (list 6 7 8 9)
+            (list 6 7 8 9)))
 
-( define (matrix-*-matrix m n) (let ((cols (transpose n))) (map ⟨??⟩ m)))
+(define v (list 1 2 3 4))
+
+
+(matrix-*-vector m v)
+
+
+(define (transpose mat) (accumulate-n cons nil mat))
+
+
+
+(define (matrix-*-matrix m n)
+    (let ((cols (transpose n)))
+        (map    (lambda (row) (map  (lambda (n-row) (dot-product  row n-row))
+                                    cols)
+                )m)
+    )
+)
+
+(matrix-*-matrix m m)
