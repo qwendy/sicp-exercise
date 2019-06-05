24353556f697ec286f3c55ce2c6ca4926b5ed13b
qianwenxiang <1542997629@qq.com>
2019-06-06 01:11:51 +0800

    wrong! to be continued

--- chapter2/Exercise 2.42.rkt
+++ chapter2/Exercise 2.42.rkt
@@ -1,18 +1,18 @@
 #lang sicp
 
 (define (enumerate-interval low high)
-  (if (> low high)
-      nil
-      (cons low (enumerate-interval (+ low 1) high))))
+    (if (> low high)
+        nil
+        (cons low (enumerate-interval (+ low 1) high))))
 
 (define (accumulate op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
 
 (define (flatmap proc seq)
-  (accumulate append nil (map proc seq)))
+    (accumulate append nil (map proc seq)))
 
 (define empty-board nil)
 
--- chapter2/Exercise 2.57.rkt
+++ chapter2/Exercise 2.57.rkt
@@ -1,4 +1,4 @@
-#lang sicp
+#lang racket
 
 (define (sum? x)
     (and (pair? x) (eq? (car x) '+)))
@@ -6,22 +6,22 @@
 (define (product? x)
     (and (pair? x) (eq? (car x) '*)))
 
-(define (make-sum a . rest)
-    (if (null? rest)
-        a
-        (+ (car rest) (make-sum a . (cdr rest)))))
+
+(define (=number? a b)
+    (if (and (number? a) (= a b))
+        #t
+        #f))
 
 
 (define (make-sum a1 . a2)
     (if (single-operand? a2)
         (let ((a2 (car a2)))
-            (cond ((=number? a1 0)
+                (cond ((=number? a1 0)
                     a2)
-                  ((=number? a2 0)
+                    ((=number? a2 0)
                     a1)
-                  ((and (number? a1) (number? a2))
+                    ((and (number? a1) (number? a2))
                     (+ a1 a2))
-                  (else
+                    (else
                     (list '+ a1 a2))))
         (cons '+ (cons a1 a2))))
-
